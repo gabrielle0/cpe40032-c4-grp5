fb1fec697124bbe0c5c73e687de55ecbecc364d7
Gabrielle Pangilinan <gabrielleapangilinan@iskolarngbayan.pup.edu.ph>
2023-01-17 19:59:20 +0800

    Group 5 - Challenge 4 - initial commit

---
+++ Assets/Challenge 4/Scripts/EnemyX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyX : MonoBehaviour
+{
+    public float speed = 100.0f;
+    private Rigidbody enemyRb;
+    private GameObject playerGoal;
+    private SpawnManagerX spawnManagerScripts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Challenge 7: Use Game Object Find to find spawn manager script
+        spawnManagerScripts = GameObject.Find("Spawn Manager").GetComponent<SpawnManagerX>();
+        enemyRb = GetComponent<Rigidbody>();
+        //Challenge 5: Change game object find to "Player Goal" and change value of speed
+        playerGoal = GameObject.Find("Player Goal");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Set enemy direction towards player goal and move there
+        Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
+        enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
+        //Challenge 7: Add force to enemies by using the enemy speed in the spawn manager script
+        enemyRb.AddForce(lookDirection * spawnManagerScripts.enemySpeed * Time.deltaTime);
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // If enemy collides with either goal, destroy it
+        if (other.gameObject.name == "Enemy Goal")
+        {
+            Destroy(gameObject);
+        } 
+        else if (other.gameObject.name == "Player Goal")
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
---
+++ Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    public ParticleSystem boostParticle;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    void Update()
+    {
+
+        // Add force to player in direction of the focal point (and camera)
+        float verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime); 
+
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);
+
+        //Challenge 6: Use if statement to get input if space bar is pressed and if vertical input is not zero
+        if (Input.GetKey(KeyCode.Space) && verticalInput != 0)
+        {
+           //Challenge 6: Add public particle system in variables, then get component and play. Also add particle effect animation prefab in
+           //inspector after making it a child object of the player
+            boostParticle.GetComponent<ParticleSystem>().Play();
+            //Challenge 6: Set speed to higher value
+            speed = 2000;
+        }
+        else
+        {
+            speed = 500;
+        }
+
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            //Challenge 3: Start Coroutine of method PowerupCooldown
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+           //Challenge 1: Subtract transform position from other game object transform position 
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position;
+
+
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
